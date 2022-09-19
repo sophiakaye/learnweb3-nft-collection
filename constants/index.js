9e1c5036d7e0f82103b839902266b66ed4d903fb
Sophia KAYA <sophiakaya@Sophias-MacBook-Pro.local>
2022-09-19 22:47:45 +0300

    lib

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x879cD80717755b56F3562259a5AEC42bFd362629";
+const WHITELIST_CONTRACT_ADDRESS = "0xAEBa971e1363Fd8F6bED2AEa583547fc1Cb76C3A";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://learnweb3-nft-collection-kappa.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
