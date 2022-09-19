aa3a3eaf07dcec26dcd07f9c5f45966931c84af5
Sophia KAYA <sophiakaya@Sophias-MacBook-Pro.local>
2022-09-19 22:26:07 +0300

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x879cD80717755b56F3562259a5AEC42bFd362629";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
